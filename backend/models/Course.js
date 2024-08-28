2a00b473aa9ab9151ce17f8a5e08b13abb33e1e2
rohantotherescue <rohantotherescue@gmail.com>
2024-08-29 01:38:45 +0530

    added duration in the model

--- backend/models/Course.js
+++ backend/models/Course.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const courseSchema = new mongoose.Schema({
   name: { type: String, required: true },
   instructor: { type: String, required: true },
+  duration: { type: Number, required: false },
   createdAt: { type: Date, default: Date.now },
 });
 
